1926e897b627b84fc66af4d187de505fff6f8ade
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2025-09-21 17:42:27 +0530

    Add JavaScript variables example

---
+++ APIs/js-api/01-variables.js
@@ -0,0 +1,9 @@
+// 📌 Variables in JavaScript
+// let = can be reassigned
+// const = cannot be reassigned
+
+let name = "Krishna";
+const age = 22;
+
+console.log("Name:", name);
+console.log("Age:", age);
