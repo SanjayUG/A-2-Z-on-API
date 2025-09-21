73408dbecf9b0cf86e73453334b83d9e3df347ae
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2025-09-21 17:43:28 +0530

    Add functions for greeting and addition in js-api

---
+++ APIs/js-api/02-functions.js
@@ -0,0 +1,12 @@
+// 📌 Functions in JavaScript
+// Functions are used to reuse code
+
+function greet(user) {
+  return `Hello, ${user}`;
+}
+
+console.log(greet("Krishna"));
+
+// Arrow function (short form)
+const add = (a, b) => a + b;
+console.log("Sum:", add(5, 3));
