e34fdaf9542ad3dc3f94d0279949d378dfcbcd44
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2025-09-21 17:44:01 +0530

    Add js-api/03-objects-json.js for JSON handling

---
+++ APIs/js-api/03-objects-json.js
@@ -0,0 +1,11 @@
+// 📌 Objects & JSON
+// JSON is the main data format used in APIs
+
+let user = {
+  id: 1,
+  name: "Krishna",
+  role: "API Engineer"
+};
+
+console.log("User Name:", user.name);
+console.log("As JSON:", JSON.stringify(user));
